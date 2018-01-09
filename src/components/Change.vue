c66eefb30e59afd81c98f9a4788c316530612af6
pratiksha <pratikshabhatale@gmail.com>
2018-01-09 15:50:26 +0530

    final

---
+++ src/components/Change.vue
@@ -0,0 +1,57 @@
+
+
+<template>
+<div class="col-md-4 ">
+<form >
+<h3>Reset:</h3>
+   <div class="form-group">
+      <label for="email">Old Passord:</label>
+      <input type="Password" class="form-control" id="oldprd" placeholder="Enter Old " name="oldprd" v-model="newusers.oldprd">
+    </div>
+    <div class="form-group">
+      <label for="newprd">New Password:</label>
+      <input type="password" class="form-control" id="newprd" placeholder="Enter new password" name="newprd" v-model="newusers.newprd">
+    </div>
+    <div class="form-group">
+      <label for="conpwd">Conform Password:</label>
+      <input type="password" class="form-control" id="conprd" placeholder="Enter conform password" name="conprd" v-model="newusers.conprd">
+    </div>
+   
+   <button type="button" name="Conform" v-on:click="Changes()" class="btn btn-success">Conform</button>
+   <br>
+  
+  </form>
+  </div>
+
+</template>
+
+
+<style scoped>
+
+</style>
+<script>
+
+    
+import axios from 'axios';
+export default {
+  name: 'Change',
+  data () {
+    return {
+     newusers:{old: '', new : '',conform:''},
+      posts: [],
+      errors: []
+    }
+  },
+methods:{
+  Changes() {
+    axios.post('/api/v1/resetPwd', { data: this.newusers })
+      .then(function (response) {
+          console.log(response);
+      })
+      .catch(function (error) {
+          console.log(error.message);
+      });
+    }
+  }
+}
+</script>
---
+++ src/components/Login.vue
@@ -0,0 +1,59 @@
+<template>
+
+
+<div class="col-md-4 loginpage">
+<h1>Login</h1>
+<form >
+   <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" v-model="newuser.email">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd" v-model="newuser.ppwrd">
+    </div>
+   
+   <button type="button" name="login" v-on:click="sublog()" class="btn btn-success">Login</button>
+   <br>
+  <router-link to="/change">Change the Password</router-link>
+  
+  </form>
+  </div>
+
+
+</template>
+
+<style scoped>
+.loginpage{
+  border:red;
+}
+h1{
+  color:red;
+  border:black;
+}
+</style>
+<script>
+import axios from 'axios';
+export default {
+  name: 'Login',
+  data () {
+    return {
+     newuser:{email: '', password : ''},
+      posts: [],
+      errors: []
+    }
+  },
+methods:{
+  sublog() {
+    axios.post('/api/v1/login', { data: this.newuser })
+      .then(function (response) {
+          console.log(response);
+      })
+      .catch(function (error) {
+          console.log(error.message);
+      });
+    }
+  }
+}
+  
+</script>
